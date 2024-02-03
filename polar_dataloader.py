bc3f46ae69a4c18b3ac975c0a23d2a3fa887543b
unknown <aleksander.cherviakov@gmail.com>
2024-02-03 13:03:33 +0300

    added class PolarSnippets

--- polar_dataloader.py
+++ polar_dataloader.py
@@ -1,15 +1,17 @@
-# py .\polar_dataloader.py --dataset_path D:\testing\learning\datasets\POLAR_dataset_100
+# sheekens home py .\polar_dataloader.py --dataset_path D:\testing\learning\datasets\POLAR_dataset_100
+# sheekens work py .\polar_dataloader.py --dataset_path C:\learning\learning\datasets\POLAR_dataset_100
 # python current_files\polar_dataloader.py --dataset_path C:\cod\datasets\POLAR_dataset_100
 import os
 import torch
 import cv2
 import numpy as np
 import argparse
-from torch.utils.data import Dataset
+from torch.utils.data import Dataset, DataLoader
 import json
 from pprint import pprint
 from varname.helpers import debug
 from cpor_snippets import square_from_rectangle
+import pathlib
 
 def parse_arguments():
     parser = argparse.ArgumentParser()
@@ -18,6 +20,29 @@ def parse_arguments():
         "--is_debug", default=0, type=int, help="enable fast run on several samples"
     )
     return parser.parse_args()
+
+# def square_snippet(img, bbox_x1y1x2y2, is_show=False):
+#     x1,y1,w,h = bbox_x1y1x2y2_to_xywh(bbox_x1y1x2y2)
+#     max_side = max(w,h)
+#     snippet = np.zeros(shape=(max_side, max_side, 3),
+#                        dtype=img.dtype)
+#     snippet[:,:,1].fill(255)
+#     snippet_x1y1x2y2 = square_from_rectangle((x1,y1,w,h))
+#     snippet_from_img = img[
+#         snippet_x1y1x2y2[1]:snippet_x1y1x2y2[3],
+#         snippet_x1y1x2y2[0]:snippet_x1y1x2y2[2],
+#         :
+#     ]
+#     snippet[
+#         :snippet_from_img.shape[0],
+#         :snippet_from_img.shape[1],
+#         :
+#     ] = snippet_from_img
+#     if is_show:
+#         cv2.imshow('snippet', snippet)
+#         cv2.waitKey(-1)
+#         cv2.destroyAllWindows()
+#     return snippet
 
 def square_snippet(img, bbox_x1y1x2y2, is_show=False):
     x1,y1,w,h = bbox_x1y1x2y2_to_xywh(bbox_x1y1x2y2)
@@ -31,12 +56,32 @@ def square_snippet(img, bbox_x1y1x2y2, is_show=False):
         snippet_x1y1x2y2[0]:snippet_x1y1x2y2[2],
         :
     ]
-    snippet[
-        :snippet_from_img.shape[0],
-        :snippet_from_img.shape[1],
-        :
-    ] = snippet_from_img
+    # cv2.imshow('snippet', snippet_from_img)
+    # cv2.waitKey(-1)
+    # cv2.destroyAllWindows()
+    # exit()
+    # snippet[
+    #     :snippet_from_img.shape[0],
+    #     :snippet_from_img.shape[1],
+    #     :
+    # ] = snippet_from_img
+    if w > h:
+        top = (w-h)//2
+        left = 0
+        # debug(w,h,top,left)
+    else:
+        top = 0
+        left = (h-w)//2
+        # debug(w,h,top,left)
+    bottom = top
+    right = left
+    # debug(bottom, right)
+    borderType = cv2.BORDER_CONSTANT
+    # value = [200,200,200]
+    # snippet = img.copy()
+    snippet = cv2.copyMakeBorder(snippet_from_img, top, bottom, left, right, borderType, value = [200,200,200])
     if is_show:
+        debug(snippet.shape)
         cv2.imshow('snippet', snippet)
         cv2.waitKey(-1)
         cv2.destroyAllWindows()
@@ -49,6 +94,33 @@ def bbox_x1y1x2y2_to_xywh(bbox_x1y1x2y2):
         abs(bbox_x1y1x2y2[2]-bbox_x1y1x2y2[0]),
         abs(bbox_x1y1x2y2[1]-bbox_x1y1x2y2[3])
     ]
+
+class PolarSnippets(Dataset):
+    def __init__(self,dataset_path, square_img_size):
+        self.dataset_path = dataset_path
+        self.img_paths = []
+        self.img_classes = []
+        self.square_img_size = square_img_size
+        for root, dirs, files in os.walk(self.dataset_path):
+            for file in files:
+                if ".png" in file:
+                    img_path = os.path.join(root, file)
+                    img_class = os.path.basename(root)
+                    self.img_paths.append(img_path)
+                    self.img_classes.append(img_class)
+        # debug(self.imgs_paths, self.img_classes)
+        # debug(len(self.imgs_paths), len(self.img_classes))
+    def __len__(self):
+        return len(self.img_paths)
+    def __getitem__(self, index):
+        img_path = self.img_paths[index]
+        img_class = self.img_classes[index]
+        img = cv2.imread(img_path)
+        square_img = cv2.resize(
+            src=img,
+            dsize=(self.square_img_size,self.square_img_size)
+        )
+        return square_img, img_class
 
 class Polar_dataset(Dataset):
     def __init__(self, dataset_path):
@@ -120,16 +192,34 @@ class Polar_dataset(Dataset):
             img = cv2.imread(img_path)
             snippet = img.copy()
             # print(img_name)
-            squared_snippet = square_snippet(snippet, self.bndbox_dict[img_name], is_show=True)
-            snippet_path = os.path.join(self.snippets_dir, f'{img_name}.png')
+            squared_snippet = square_snippet(snippet, self.bndbox_dict[img_name], is_show=False)
+            snippet_class_path = os.path.join(self.snippets_dir, self.img_classes_dict[img_name])
+            ensure_folder(snippet_class_path)
+            snippet_path = os.path.join(snippet_class_path, f'{img_name}.png')
             cv2.imwrite(snippet_path, squared_snippet)
             print(snippet_path)
 
+def ensure_folder(dir_fname):
+    if not os.path.exists(dir_fname):
+        try:
+            pathlib.Path(dir_fname).mkdir(parents=True, exist_ok=True)
+        except PermissionError:
+            print('Unable to create {} directory. Permission denied'.format(dir_fname))
+            return False
+    return os.path.exists(dir_fname)
 
 if __name__ == "__main__":
     arguments = parse_arguments()
     dataset_path = arguments.dataset_path
     is_debug = arguments.is_debug
 
-    polar_dataset = Polar_dataset(dataset_path)
+    # polar_dataset = Polar_dataset(dataset_path)
+    polar_snippets_dataset = PolarSnippets(dataset_path, 228)
+    polar_snippets_dataloader = DataLoader(
+        dataset=polar_snippets_dataset,
+        batch_size=2
+    )
+    for img_batch, label_batch in polar_snippets_dataloader:
+        debug(img_batch.size(), label_batch)
+        exit()
     # polar_dataset.visualize_bboxes()
