646e6ed5616b5024ac5a00f2475aaa7719ad4f78
sheekens <aleksander.cherviakov@gmail.com>
2024-03-11 21:53:24 +0300

    manual accuracy estimation added

--- train.py
+++ train.py
@@ -7,6 +7,7 @@ from torch.utils.data import Dataset, DataLoader
 from simple_convolution_model import Simple2DConv
 from varname.helpers import debug
 from sklearn.metrics import confusion_matrix
+import time
 
 
 dataset_path = 'D:/testing/learning/datasets/POLAR_dataset_100'
@@ -25,6 +26,10 @@ epochs = 9000
 classes_indexes_array = np.arange(0,len(polar_snippets_dataset.classes_names),1)
 # debug(classes_indexes_array)
 # debug(polar_snippets_dataset.classes_names())
+t1 = time.time()
+accuracy = 0
+tp_tn = 0
+pred_sum = 0
 for epoch in range(epochs):
     total_conf_matrix = None
     for img_batch, label_batch in polar_snippets_dataloader:
@@ -50,5 +55,24 @@ for epoch in range(epochs):
         # debug(out, out_probabilities)
         # debug(out.size())
         # exit()
-    debug(total_conf_matrix)
-    exit()
+    if epoch == 20:
+        # debug(len(total_conf_matrix))
+        debug(total_conf_matrix)
+        t2 = time.time()
+        print('done by [{:.03f}s]\n'.format((t2 - t1)))
+        row_num = 0
+        for true_class in total_conf_matrix:
+            # debug(true_class)
+            # debug(row_num)
+            col_num = 0
+            for prediction_class in true_class:
+                # debug(prediction_class)
+                # debug(col_num)
+                pred_sum += prediction_class
+                if col_num == row_num:
+                    tp_tn += prediction_class
+                col_num += 1
+            row_num += 1
+        accuracy = tp_tn / pred_sum
+        print('accuracy [{:.03f}%]\n'.format((accuracy*100)))
+        exit()
