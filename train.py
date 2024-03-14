c640a89252fa6d2817e74ed8dbfe2d86ce292704
sheekens <aleksander.cherviakov@gmail.com>
2024-03-14 20:53:07 +0300

    train learning theory

--- train.py
+++ train.py
@@ -12,9 +12,9 @@ import time
 # from evidently.report import Report
 
 
-# dataset_path = 'D:/testing/learning/datasets/POLAR_dataset_100' ##sheekens home
-dataset_path = 'C:/testing/learning/datasets/POLAR_dataset_100' ##sheekens work
-polar_snippets_dataset = PolarSnippets(dataset_path, 24)
+dataset_path = 'D:/testing/learning/datasets/POLAR_dataset_100' ##sheekens home
+# dataset_path = 'C:/testing/learning/datasets/POLAR_dataset_100' ##sheekens work
+polar_snippets_dataset = PolarSnippets(dataset_path, square_img_size=24)
 polar_snippets_dataloader = DataLoader(
     dataset=polar_snippets_dataset,
     batch_size=2
@@ -31,6 +31,8 @@ classes_indexes_array = np.arange(0,len(polar_snippets_dataset.classes_names),1)
 # debug(polar_snippets_dataset.classes_names())
 t1 = time.time()
 accuracy = 0
+precision = {}
+recall = {}
 tp = []
 fp = []
 fn = []
@@ -59,38 +61,42 @@ for epoch in range(epochs):
         # debug(out, out_probabilities)
         # debug(out.size())
         # exit()
-    if epoch == 0:
+    if epoch == 2:
         t2 = time.time()
-        print('epoching done by [{:.03f}s]\n'.format((t2 - t1)))
+        print('epoching done by [{:.02f}s]\n'.format((t2 - t1)))
         row_num = 0
         for true_class in total_conf_matrix:
-            # debug(true_class)
             # debug(row_num)
             col_num = 0
-            for prediction_class in true_class:
-                # debug(prediction_class)
+            for predicted_class in true_class:
+                # debug(predicted_class)
                 if len(fp) < col_num+1:
                     fp.append(0)
                 if len(tp) < col_num+1:
                     tp.append(0)
                 if len(fn) < col_num+1:
                     fn.append(0)
-                # debug(prediction_class)
+                # debug(predicted_class)
                 # debug(col_num)
-                pred_sum += prediction_class
+                pred_sum += predicted_class
                 if col_num == row_num:
-                    tp[col_num] += prediction_class
+                    tp[col_num] += predicted_class
                 ##TODO: make omerica great again or
-                ##remake tp as fp by classes
+                ##remake tp as fp by classes +
                 ##add fn as fp by classes
-                else:
-                    fp[col_num] += prediction_class
+                if col_num != row_num:
+                    fp[col_num] += predicted_class
+                    fn[row_num] += predicted_class
                 col_num += 1
             row_num += 1
-        debug(total_conf_matrix)
-        debug(tp)
-        debug(sum(tp))
-        accuracy = sum(tp) / pred_sum
-        print('accuracy [{:.03f}%]\n'.format((accuracy*100)))
+        print(total_conf_matrix)
         debug(fp)
+        debug(tp)
+        debug(fn)
+        accuracy = sum(tp) / pred_sum
+        for true_class in range(len(total_conf_matrix)):
+            precision[true_class] = (tp[true_class] / (tp[true_class] + fp[true_class]))*100
+            recall[true_class] = (tp[true_class] / (tp[true_class] + fn[true_class]))*100
+        print('accuracy [{:.02f}%]\n'.format((accuracy*100)))
+        debug(precision, recall)
         exit()
